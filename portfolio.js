b5b44590ea2cbedf5050859293afa00dc155bd13
Ben Davis <benjaminedavis@gmail.com>
2015-10-09 16:18:19 -0700

    jquery fadein document

--- portfolio.js
+++ portfolio.js
@@ -1,18 +1,17 @@
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  $(document).ready(function() {
+    $('#fullpage')
+      .css('visibility', 'visible')
+      .animate({opacity: 1.0, top: '40px'}, 4000);
+});
 
   Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+
   });
 
   Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+
   });
 }
 
